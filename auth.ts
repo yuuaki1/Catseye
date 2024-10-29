7ff74b247580febc1bec275eb919b509a1c2d5eb
Arihant Sharma <yuudaiakihiro1@gmail.com>
2024-10-29 18:10:08 +0530

    barebones

--- auth.ts
+++ auth.ts
@@ -1,9 +1,16 @@
-import { github, google } from "better-auth/social-providers";
 import NextAuth from "next-auth";
+import google from "next-auth/providers/google";
+import github from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
-        google, 
-        github
+        google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET
+        }), 
+        github({
+            clientId: process.env.AUTH_GITHUB_ID,
+            clientSecret: process.env.AUTH_GITHUB_SECRET
+        })
     ]
 })
